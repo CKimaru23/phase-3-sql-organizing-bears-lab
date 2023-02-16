573c8be08e323ff16fa47f6d46bf454766ba38e9
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2023-02-16 20:01:30 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 15, 'M', 'Brown', 'Chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 7, 'M', 'Black', 'Hyper', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 26, 'F', 'White', 'Calm', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 52, 'M', 'Grey', 'Grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 10, 'F', 'Dark Brown', 'Hyper', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 34, 'M', 'Black', 'Unreasonable', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 15, 'F', 'Brown', 'Chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 30, 'F', 'Black', 'Angry', 0); 
